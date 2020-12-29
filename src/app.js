23dd7e688630aff158fdcf20581c3ef98ef3c1c4
Akshat Gadhwal <akshatgadhwal1@gmail.com>
2020-12-30 01:36:12 +0530

    fgdfg

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 // app.set("view engine","html");
 app.engine('ejs', require('ejs').__express);
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // app.use(express.static('../'));
