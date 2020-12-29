b095f3ea9c15b07804f1e37245be949a11db94eb
Akshat Gadhwal <akshatgadhwal1@gmail.com>
2020-12-30 00:12:09 +0530

    moved the content to the functions folder

--- src/app.js
+++ src/app.js
@@ -5,17 +5,14 @@ const serverless = require("serverless-http");
 
 const router = express.Router();
 
-app.set("view engine","html");
-// app.engine('ejs', require('ejs').__express);
+// app.set("view engine","html");
+app.engine('ejs', require('ejs').__express);
 
-app.use(express.static('../'));
-app.engine('html', require('ejs').renderFile);
+// app.use(express.static('../'));
+// app.engine('html', require('ejs').renderFile);
 
 router.get('/',(req,res) => {
-    res.json({
-        'Developed by: ':"Akshat",
-        'Issue: ':"it can only be a single page website :)"
-    });
+    res.render('home');
 });
 
 app.use('/.netlify/functions/app',router);
